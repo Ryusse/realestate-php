f06d8c7d4860751978f0a0246212636c2820bb14
jocano <joelangelocano@gmail.com>
2025-08-03 19:40:40 -0500

    feat install flyonui

--- pages/index.php
+++ pages/index.php
@@ -4,18 +4,13 @@ require('system/main.php');
 $layout = new HTML(title: 'PHP via Vite');
 ?>
 
-<div class="flex flex-col items-center gap-10 text-2xl">
-	<?php include('partials/nav.php'); ?>
-
-	<div class="flex flex-col items-center">
-		<?= VITE_NAME; ?>
-
-		<div>+</div>
-
-		<img src="%BASE%/logo.svg" class="w-20" />
-	</div>
-
-	<div id="repos" class="text-base flex gap-10"></div>
-</div>
-
+<main>
+	<section>
+		<h1>Welcome to PHP via Vite</h1>
+		<p>This is a simple setup to demonstrate how PHP can work with Vite for modern web development.</p>
+	</section>
+</main>
+<!-- FlyonUI Components -->
+<script src="../node_modules/flyonui/flyonui.js"></script>
+<!-- FlyonUI Components End -->
 <script src="/src/scripts/repos.ts" type="module"></script>
